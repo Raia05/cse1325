e4966e1ff202c585875b7d8e799b00e42382554b
Raia <arefeen.nafi@gmail.com>
2024-08-21 11:33:16 -0500

    P01 extreme bonus draft 1 (only has 1 image)

---
+++ P01/extreme_bonus/Hello.java
@@ -0,0 +1,10 @@
+
+
+public class Hello 
+{
+    public static void main(String[] args)
+    {
+        String name = System.getProperty("user.name");
+        System.out.println("Hello " + name);
+    }
+}
