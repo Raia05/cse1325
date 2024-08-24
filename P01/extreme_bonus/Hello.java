312d3181636dbce82a8bfb8a92182d66291a3ff9
Raia <arefeen.nafi@gmail.com>
2024-08-24 16:59:51 -0500

    added descriptive comment to extreme bonus P01

--- P01/extreme_bonus/Hello.java
+++ P01/extreme_bonus/Hello.java
@@ -5,6 +5,8 @@ public class Hello
     public static void main(String[] args)
     {
         String name = System.getProperty("user.name");
+        // uses the system property of the user name to get the name rather than passed in input
+
         System.out.println("Hello " + name);
     }
 }
