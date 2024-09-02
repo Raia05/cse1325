bb63dfadcb248b532a9bf48404cbc3344af17364
Raia <arefeen.nafi@gmail.com>
2024-09-02 14:22:45 -0500

    P02 bonus fixed Denomination enum to match the bonus formatting

--- P02/bonus/Denomination.java
+++ P02/bonus/Denomination.java
@@ -1,7 +1,26 @@
 public enum Denomination
 {
-    PENNY,
-    NICKEL,
-    DIME,
-    QUARTER
+    PENNY(0.01),
+    NICKEL(0.05),
+    DIME(0.10),
+    QUARTER(0.25);
+
+
+    private double value;
+
+    private Denomination(double value)
+    {
+        this.value = value;
+    }
+
+    public double getValue()
+    {
+        return this.value;
+    }
+
+    @Override
+    public String toString()
+    {
+        return name().toLowerCase();
+    }
 }
