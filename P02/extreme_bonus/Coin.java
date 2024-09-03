8bd0fc55e4a0b589e8edd223189510abb0f01226
Raia <arefeen.nafi@gmail.com>
2024-09-02 19:16:38 -0500

    added getWeight() method to Coin implimented with a case statement

--- P02/extreme_bonus/Coin.java
+++ P02/extreme_bonus/Coin.java
@@ -19,6 +19,31 @@ public class Coin
         return this.year;
     }
 
+    public double getWeight()
+    {
+        double weight = 0; 
+        switch(this.denomination)
+        {
+            case PENNY:
+                weight = (this.year < 1983) ? 3.110 : 2.500;
+                break;
+            case NICKEL:
+                weight = 5.000;
+                break;
+            case DIME:
+                weight = (this.year < 1965) ? 2.500 : 2.268;
+                break;
+            case QUARTER:
+                weight = (this.year < 1965) ? 6.250 : 5.670;
+                break;
+
+            default:
+                weight = 0;
+        }
+
+        return weight;
+    }
+
     @Override
     public String toString()
     {
