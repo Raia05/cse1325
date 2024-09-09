12bde409ac06c92e85b57bc7c56ec91ee89cae75
Raia <arefeen.nafi@gmail.com>
2024-09-08 21:54:14 -0500

    P03 created student class with all methods and values specifcied

---
+++ P03/Student.java
@@ -0,0 +1,30 @@
+public class Student
+{
+    private String name;
+    private int id;
+    private String email;
+    private Account account;
+
+    Student(String name, int id, String email)
+    {
+        this.name = name;
+        this.id = id;
+        if(!(email.endsWith("@uta.edu") || email.endsWith("@mavs.uta.edu")))
+            throw new IllegalArgumentException("Non-UTA email address: " + email);
+        this.email = email;
+
+        this.account = Account();
+
+    }
+
+    public String requestMedia(Media media)
+    {
+        return this.account.play(media);
+    }
+
+    @Override
+    public String toString()
+    {
+        return this.name + " (" + this.id ", " + this.email + ", Account #" + this.account.getAccountNumber() + ")";
+    }
+}
