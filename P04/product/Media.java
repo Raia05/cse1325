be3c777712960592cfd2e9ecbc90233c9b8205f4
Raia <arefeen.nafi@gmail.com>
2024-09-16 12:37:05 -0500

    P04 added the javadoc documentation to the file

--- P04/product/Media.java
+++ P04/product/Media.java
@@ -1,11 +1,28 @@
 package product;
 
+
+/**
+ * 
+ * Models a Media instance for the Moes system using a title, url, and how many points it costs.
+ * 
+ * 
+ * @version 1.0
+ * @since 1.0
+ * @author Nafiul Arefeen
+ */
 public class Media
 {
     private String title;
     private String url;
     private int points;
 
+    /**
+     * Constructs a Media object with the specified title, URL, and points.
+     * 
+     * @param title  the title of the media
+     * @param url    the URL associated with the media
+     * @param points the points or rating assigned to the media
+     */
     public Media(String title, String url, int points)
     {
         this.title = title;
@@ -13,12 +30,22 @@ public class Media
         this.points = points;
     }
 
+    /**
+     * Returns the points or cost of the media.
+     * 
+     * @return the point cost of the media
+     */
     public int getPoints()
     {
         return this.points;
     }
 
-
+    /**
+     * Returns a string representation of the media in the format 
+     * "{title} ({url}, {points} points)".
+     * 
+     * @return a formatted string representing the media
+     */
     @Override
     public String toString()
     {
