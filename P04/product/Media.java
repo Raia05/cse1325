ab25d3902826d327b2e73b09c86a479677c9679e
Raia <arefeen.nafi@gmail.com>
2024-09-15 19:02:22 -0500

    created changes needed in media (not javadoc yet)

--- P04/product/Media.java
+++ P04/product/Media.java
@@ -4,11 +4,18 @@ public class Media
 {
     private String title;
     private String url;
+    private int points;
 
-    public Media(String title, String url)
+    public Media(String title, String url, int points)
     {
         this.title = title;
         this.url = url;
+        this.points = points;
+    }
+
+    public int getPoints()
+    {
+        return this.points;
     }
 
 
