97c0ac05d8b75e4bdc1244dd9d2308ab1fbf5146
Raia <arefeen.nafi@gmail.com>
2024-09-15 22:26:42 -0500

    P04 created arrayLists and Media methods for Moes class

--- P04/moes/Moes.java
+++ P04/moes/Moes.java
@@ -1,9 +1,57 @@
 package moes;
 
+import java.util.ArrayList;
 import customer.Student;
 import product.Media;
 
 public class Moes
 {
-    
+    private ArrayList<Media> library = new ArrayList<>();
+    private ArrayList<Student> customers = new ArrayList<>();
+
+    public void addMedia(Media media)
+    {
+        this.library.add(media);
+    }
+
+    public String getMediaList()
+    {
+        StringBuilder mediaList = new StringBuilder();
+
+        for(int i = 0; i < this.library.size(); i++)
+        {
+            mediaList.append(i).append(") ").append(this.library.get(i).toString()).append("\n");
+        }
+
+        return mediaList.toString();
+
+    }
+
+    public void addStudent(Student student)
+    {
+        this.customers.add(student);
+    }
+
+    public String getStudentList()
+    {
+
+    }
+
+    public int getPoints(int studentIndex)
+    {
+
+    }
+
+    public String buyPoints(int studentIndex, int points)
+    {
+
+    }
+
+    public String playMedia(int studentIndex, int mediaIndex)
+    {
+
+    }
+
+
+
 }
