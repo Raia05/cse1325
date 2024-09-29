549121969588639bf89e828b53acdb3b6dfd1794
Raia <arefeen.nafi@gmail.com>
2024-09-29 14:43:21 -0500

    P05 created Main class file in mdi and finished student methods and began signitures for media methods

---
+++ P05/mdi/Main.java
@@ -0,0 +1,63 @@
+package mdi;
+import moes.Moes;
+import java.util.Scanner;
+import customer.Student;
+
+public class Main
+{
+    private Moes moes;
+    private String output;
+    private Menu menu;
+    private boolean running;
+
+    private void addStudent()
+    {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Student name? ");
+        String name = in.nextLine();
+
+        System.out.print("Student ID? ");
+        int id = in.nextInt();
+
+        System.out.print("Student email? ");
+        String email = in.nextLine();
+
+        System.out.print("(a)lacarte or (u)nlimited? ");
+        char account = in.next().toCharArray()[0];
+
+        if (account == 'a')
+            boolean unlimited = false;
+        else if (account == 'u')
+            boolean unlimited = true;
+        else
+            throw new IllegalArgumentException("unknown Account type specified");
+
+        Student student = new Student(name, id, email, unlimited);
+
+        this.moes.addStudent(student);
+
+    }
+
+    private void listStudents()
+    {
+        System.out.println(this.moes.getStudentList());
+    }
+
+    private void addMedia()
+    {
+
+    }
+
+    private void playMedia()
+    {
+
+    }
+
+    private void listMedia()
+    {
+        
+    }
+
+    
+}
