6158e4984bc9b1736b5de8bcb6fec829c1495b59
Raia <arefeen.nafi@gmail.com>
2024-09-30 12:23:29 -0500

    P05 added most methods for Main but started helper prompt methods

--- P05/mdi/Main.java
+++ P05/mdi/Main.java
@@ -10,10 +10,10 @@ public class Main
     private String output;
     private Menu menu;
     private boolean running;
+    private Scanner in = new Scanner(System.in);
 
     private void addStudent()
     {
-        Scanner in = new Scanner(System.in);
 
         System.out.print("Student name? ");
         String name = in.nextLine();
@@ -47,7 +47,6 @@ public class Main
 
     private void addMedia()
     {
-        Scanner in = new Scanner(System.in);
 
         System.out.print("Title? ");
         String title = in.nextLine();
@@ -67,6 +66,14 @@ public class Main
     private void playMedia()
     {
 
+        System.out.print("Student number? ");
+        int student = in.nextInt();
+
+        System.out.print("Media number? ");
+        int media = in.nextInt();
+
+        this.moes.playMedia(student, media);
+
     }
 
     private void listMedia()
@@ -77,12 +84,119 @@ public class Main
     private void AvailablePoints()
     {
 
+        System.out.print("Student number? ");
+        int student = in.nextInt();
+
+        this.moes.getPoints(student);
+
+
     }
 
     private void buyPoints()
     {
+
+        System.out.print("Student number? ");
+        int student = in.nextInt();
+
+        System.out.print("Points? ");
+        int points = in.nextInt();
+
+        this.moes.buyPoints(student, points);
         
     }
+
+    public Main()
+    {
+        this.running = true;
+        this.moes = new Moes();
+        this.menu = new Menu();
+        this.output = "";
+
+        this.menu.addMenuItem(new MenuItem("Exit", () -> endApp()));
+        this.menu.addMenuItem(new MenuItem("Play media", () -> playMedia()));
+        this.menu.addMenuItem(new MenuItem("List media", () -> listMedia()));
+        this.menu.addMenuItem(new MenuItem("List available points", () -> AvailablePoints()));
+        this.menu.addMenuItem(new MenuItem("Buy points", () -> buyPoints()));
+        this.menu.addMenuItem(new MenuItem("Add media", () -> addMedia()));
+        this.menu.addMenuItem(new MenuItem("List all students", () -> listStudents()));
+        this.menu.addMenuItem(new MenuItem("Add a student", () -> addStudent()));
+
+
+
+
+
+
+
+
+    }
+
+    public static void main(String[] args) 
+    {
+        
+    }
+
+    private void mid()
+    {
+        while(this.running)
+        {
+            try
+            {
+                int i;
+                if (i == null) continue;
+                menu.run(i);
+            }
+            catch (Exception e)
+            {
+                print("#### Invalid command");
+            }
+        }
+
+    }
+
+    private void endApp()
+    {
+        this.running = false;
+        
+    }
+
+    // helper methods
+    public static String getString(String prompt, String cancelInput, String defualtInput)
+    {
+        String s = null;
+        while(true)
+        {
+            try
+            {
+                System.out.print(prompt);
+                s = in.nextLine().trim();
+                if(s.isEmpty() && defualtInput != null) s = defualtInput;
+                break;
+            }
+            catch(Exception e)
+            {
+                System.err.println("Invalid input!");
+            }
+            if(cancelInput != null && s.equals(cancelInput)) s = null;
+            return s;
+
+        }
+    }
+
+
+    public static String getString(String prompt, String cancelInput) 
+    {
+        return getString(prompt, cancelInput, null);
+    }
+    
+    public static String getString(String prompt) 
+    {
+        return getString(prompt, null, null);
+    }
+
+
+
+
+
 
     
 }
