a829a171ea1e9929053c49e30c10c21229a08e94
Raia05 <arefeen.nafi@gmail.com>
2024-11-03 17:26:25 -0600

    P09 created signitures for class Clock

---
+++ P09/full_credit/clock.h
@@ -0,0 +1,20 @@
+#ifndef __CLOCK_H
+#define __CLOCK_H
+
+
+#include <iostream>
+#include <iomanip>
+
+class Clock
+{
+    protected:
+        int _hours, _minutes, _seconds;
+        
+    public:
+        Clock(int hours, int minutes, int seconds);
+        virtual ~Clock();
+        void tic();
+        void print();
+};
+
+#endif
